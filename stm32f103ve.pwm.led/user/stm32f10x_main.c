95b6bd8cf00f2d3d8fd63a6fa31ddb5f6f53696e
e <mz8023yt@163.com>
2017-05-21 03:48:48 +0800

    feature: realize the led driver
    1. 实现了 led 灯的驱动程序

--- stm32f103ve.pwm.led/user/stm32f10x_main.c
+++ stm32f103ve.pwm.led/user/stm32f10x_main.c
@@ -1,9 +1,12 @@
 #include "stm32f10x.h"
+#include "bsp_led.h"
 
 int main(void)
 {
-
-	while(1)                            
-	{
-	}
+        led_init();
+        
+        while(1)
+        {
+                led_light_color(0x0000ff);
+        }
 }

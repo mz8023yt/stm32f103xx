70031d3b812d9bbbb03833ca87984a0055ac25e6
e <mz8023yt@163.com>
2017-06-10 14:59:58 +0800

    style: rename the code files name
    1. 对于驱动文件进行了重新命名的操作

--- stm32f103ve.usart.waveform/driver/waveform/data_scope.c
+++ stm32f103ve.usart.waveform/driver/waveform/data_scope.c
@@ -1,7 +1,7 @@
-#include "mini_balance.h"
+#include "data_scope.h"
 
 /* 串口发送的数据帧的帧缓冲区 */
-u8 mini_balance_buffer[42] = {0};
+u8 data_scope_buffer[42] = {0};
 
 /**
  * @brief 将单精度浮点数据转成 4 字节数据并存入指定地址
@@ -10,7 +10,7 @@ u8 mini_balance_buffer[42] = {0};
  * @param begin  指定保存在 buffer 中的位置
  * @return null
  */
-void mini_balance_float_to_byte(float *target, u8 *buffer, u8 begin)
+void data_scope_float_to_byte(float *target, u8 *buffer, u8 begin)
 {
         /* 得到 float 数据的首地址 */
         u8 *point = (u8*) target;
@@ -25,26 +25,26 @@ void mini_balance_float_to_byte(float *target, u8 *buffer, u8 begin)
  * @param channel 指定此但精度浮点数对应的显示通道 (1 - 10)
  * @param data 传入的单精度浮点数, 上位机显示的就是它
  * @return null
- * @note mini balance 上位机最多支持 10 个通道
+ * @note data_scope 上位机最多支持 10 个通道
  */
-void mini_balance_write_buffer(u8 channel, float data)
+void data_scope_write_buffer(u8 channel, float data)
 {
         if ((channel >= 1) && (channel <= 10))
-                mini_balance_float_to_byte(&data, mini_balance_buffer, 4 * channel - 3);
+                data_scope_float_to_byte(&data, data_scope_buffer, 4 * channel - 3);
 }
 
 /**
- * @brief 生成 mini balance 上位机能正确识别的帧格式
+ * @brief 生成 data_scope 上位机能正确识别的帧格式
  * @param channel 指定本次格式化帧中使用到了多少个通道 (1 - 10)
  * @return succed: 发送缓冲区数据个数 fail: -1
  */
-int mini_balance_ready_to_send(u8 channel)
+int data_scope_ready_to_send(u8 channel)
 {
         if ((channel >= 1) && (channel <= 10))
         {
                 /* 一帧数据帧以 $ 符号开始 */
-                mini_balance_buffer[0] = '$';
-                mini_balance_buffer[4 * channel + 1] = 4 * channel + 1;
+                data_scope_buffer[0] = '$';
+                data_scope_buffer[4 * channel + 1] = 4 * channel + 1;
                 return (4 * channel + 2);
         }
         
---
+++ stm32f103ve.usart.waveform/driver/waveform/data_scope.h
@@ -0,0 +1,11 @@
+#ifndef __DATA_PRTOCOL_H
+#define __DATA_PRTOCOL_H
+
+#include "stm32f10x.h"
+
+extern u8 data_scope_buffer[42];                                // 待发送帧数据缓存区
+
+void data_scope_write_buffer(u8 channel, float data);           // 写通道数据至待发送帧数据缓存区
+int data_scope_ready_to_send(u8 channel);                       // 发送帧数据生成函数
+
+#endif 
